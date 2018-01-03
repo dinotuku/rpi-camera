7070dcb1ba6ad28709ea0081b5c98900adc600e9
dtk0528 <tungkuan0528@gmail.com>
2018-01-03 10:12:44 +0800

    Set file max age to 0

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ else:
     sys.exit()
 
 app = Flask(__name__)
-# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-# app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 camera = Camera()
 
